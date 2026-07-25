b2a347d80f4b780ba9f56d415f841a688e437edd
Furkan Eroğlu <furkaneroglu0377@gmail.com>
2026-07-25 20:12:33 +0300

    Airflow webserverinda olusturulan 'SIMULASYON_TARIHI' adli variable'i otomatik olaraktan 1 gun ileri saracak task yazildi.

--- dags/ecommerce-pipeline.py
+++ dags/ecommerce-pipeline.py
@@ -1,9 +1,12 @@
 #amacimiz yazilan python scriptlerini airflowun yonetebilecegi tasklara bolme ve birbirine baglama
-
+from airflow.cli.cli_config import TASKS_COMMANDS
 from airflow.decorators import dag, task
 from datetime import datetime
 import os
 import sys
+from airflow.models import Variable
+import pandas as pd
+
 
 
 """
@@ -16,7 +19,6 @@ Script dosyaların: /opt/airflow/scripts/ klasöründedir.
 
 Benim airflowda bu scriptin bulundugu dizinin bir ust dizine cikmam gerekirki process_datayi gorsun
 """
-
 sys.path.append('/opt/airflow')
 from scripts.process_data import clean_and_summarize_data
 from scripts.ai_action import generate_ai_insight
@@ -31,11 +33,11 @@ beraber arayuze yansitir .
 """
 
 @dag(
-    dag_id='ecommerce_sales_pipeline_modular',     #arayuzde sol kisimda gozukecek benzersiz dag adi
+    dag_id='ecommerce_time_machine_pipeline',     #arayuzde sol kisimda gozukecek benzersiz dag adi
     start_date=datetime(2023,1,1),   #BUNU YAZMAK ZORUNLU
     schedule=None,    #zamansiz calisacak manuel olarak yani
     catchup=False,    #(True :start datein belirttigi tarihten bu yana gecmis gunler icin de calisir), gecmise bakma demis oluyoruz
-    tags=["ecommerce", "etl", "best_practice"]
+    tags=["ecommerce", "etl", "best_practice", "llm", "time_windowing"]
 )
 
 def ecommerce_pipeline():
@@ -54,14 +56,29 @@ def ecommerce_pipeline():
         """scripts/process_data.py içindeki clean_and_summarize_data fonksiyonunu çalıştırır."""
         clean_and_summarize_data()
 
+
+    #TASK 3: VERILER ICIN YZ CIKTISINI ALMA
     @task
     def ai_insight_task():
         generate_ai_insight()
 
 
+    @task
+    def advance_simulation_date():
+        current_date_str = Variable.get("SIMULASYON_TARIHI", default_var="2010-12-01")
+        #Bu sekilde tarih kisminda herhangi bir yanlislik yasanmiyormus!
+        current_date = pd.to_datetime(current_date_str)
+        next_date = current_date + pd.Timedelta(days=1)
+        next_date_str = next_date.strftime("%Y-%m-%d")
+
+        Variable.set("SIMULASYON_TARIHI", next_date_str) #set!
+        print(f"Zaman 1 gun ileri alindi: Sistem {current_date_str} tarihinden {next_date_str} tarihine başarıyla geçirildi!")
+
+
+
 
     #TASK BAGIMLILIKLARI
-    check_raw_data() >> process_data_task() >> ai_insight_task()
+    check_raw_data() >> process_data_task() >> ai_insight_task() >> advance_simulation_date()
     #right shift (>>) operatoru gorevlerin sirasini belirler
 
 #DAG'i tanimla ve baslat! SART(Normal fonk cagirmak gibi)
